505bef2c7ed84fd321c6c9a2b50db13c503d0ddf
quoctrong0303 <quoctrong0303@gmail.com>
2023-06-04 20:11:52 +0700

    fixed image

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -28,7 +28,7 @@ const MovieList: React.FC<Props> = ({ handleLoadMovie }) => {
                         >
                             <img
                                 crossOrigin="anonymous"
-                                src={`${movie.poster_url}`}
+                                src={`https://img.ophim1.com/uploads/movies/${movie.poster_url}`}
                                 className=" h-4/5 object-contain w-full  cursor-pointer"
                                 onClick={() =>
                                     nagivate(`/xem-phim/${movie.slug}`)
--- src/components/SlideMovie/SlideMovie.tsx
+++ src/components/SlideMovie/SlideMovie.tsx
@@ -63,7 +63,7 @@ const SlideMovie: React.FC<Props> = ({ handleLoadMovie }) => {
                         >
                             <img
                                 crossOrigin="anonymous"
-                                src={`${movie.poster_url}`}
+                                src={`https://img.ophim1.com/uploads/movies/${movie.thumb_url}`}
                                 className=" h-4/5 object-contain w-full  cursor-pointer"
                                 onClick={() => handleLoadMovie(movie.slug)}
                             />
